438d50ef8245a4a386e3a18be3fa5fd0dc11b9d8
Fabian Bitter <fabian@bitter.de>
2025-04-10 18:53:59 +0200

    Update

--- controller.php
+++ controller.php
@@ -9,7 +9,7 @@ use Concrete\Core\Package\Package;
 class Controller extends Package
 {
     protected string $pkgHandle = 'multimedia_slideshow';
-    protected string $pkgVersion = '1.0.4';
+    protected string $pkgVersion = '0.0.1';
     protected $appVersionRequired = '9.0.0';
     protected $pkgAutoloaderRegistries = [
         'src/Bitter/MultimediaSlideshow' => 'Bitter\MultimediaSlideshow',
